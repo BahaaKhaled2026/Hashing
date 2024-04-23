8e4334a0459b0fcab5d31ab25119ee6cf3023a60
BahaaKhaled2026 <bahaa.eng2026@gmail.com>
2024-04-23 15:57:02 +0200

    ON2

--- src/PerfectHashTableON2.java
+++ src/PerfectHashTableON2.java
@@ -4,14 +4,15 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class PerfectHashTableON2<E>{
+public class PerfectHashTableON2<E> {
     private int numberOfCollisions;
     private int counter;
     private int size; // Size of the hash table
     int[][] hashMatrix; // Hash matrix for the hashing
     final private double loadFactor = 0.6; // Load factor for resizing
     E[] table;
-    Map<E, Integer> elements = new HashMap<>();
+    E[] tempTable;
+    int itemCounter;
     long time;
 
     public PerfectHashTableON2() {
@@ -34,26 +35,38 @@ public class PerfectHashTableON2<E>{
 
     // Method to perform perfect hashing using the O(N^2) method
     public void createPerfectHashTable() {
+        itemCounter=0;
         boolean collision;
         counter = 0;
         long startTime = System.currentTimeMillis();
+        E[] tempTable = (E[]) new Object[size];
+        int j = 0;
+        for (int i = 0; i < this.table.length; i++) {
+            if (this.table[i] != null) {
+                tempTable[j++] = this.table[i];
+                this.itemCounter++;
+            }
+        }
         do {
             collision = false;
             int[][] hashFunction = HashingFunctions.generateHashMatrix(this.size); // Generate random hash function
-            E[] table = (E[]) new Object[size];
-            for (E item : elements.keySet()) {
-                int key = item.hashCode();
-                int hashValue = HashingFunctions.multiplyMatrix(hashFunction, HashingFunctions.decimalToBinary(key)) % size;
-                if (table[hashValue] != null) {
-                    collision = true;
-                    counter++;
-                    break;
+            E[] ansTable = (E[]) new Object[size];
+            for (E item : tempTable) {
+                if (item != null) {
+                    int key = item.hashCode();
+                    int hashValue = HashingFunctions.multiplyMatrix(hashFunction, HashingFunctions.decimalToBinary(key))
+                            % size;
+                    if (ansTable[hashValue] != null) {
+                        collision = true;
+                        counter++;
+                        break;
+                    }
+                    ansTable[hashValue] = item;
                 }
-                table[hashValue] = item;
             }
             if (!collision) {
                 this.hashMatrix = hashFunction;
-                this.table = table;
+                this.table = ansTable;
             }
         } while (collision);
         long endTime = System.currentTimeMillis();
@@ -61,9 +74,8 @@ public class PerfectHashTableON2<E>{
     }
 
     public void insert(E input) {
-        elements.put(input, 1);
         time = 0;
-        if (elements.size() > size * loadFactor) {
+        if (itemCounter > size * loadFactor) {
             size *= size;
             createPerfectHashTable();
         }
@@ -71,25 +83,34 @@ public class PerfectHashTableON2<E>{
         int hashValue = HashingFunctions.multiplyMatrix(hashMatrix, HashingFunctions.decimalToBinary(key)) % size;
         if (table[hashValue] == null) {
             table[hashValue] = input;
+            itemCounter++;
         } else {
-            System.out.println("Collision detected");
-            this.numberOfCollisions++;
-            createPerfectHashTable();
+            if (table[hashValue].equals(input)) {
+                System.out.println("Duplicate Item");
+            } else {
+                for(int i=0;i< table.length;i++){
+                    if (table[i]==null){
+                        table[i]=input;
+                        break;
+                    }
+                }
+                System.out.println("Collision detected");
+                this.numberOfCollisions++;
+                createPerfectHashTable();
+            }
         }
     }
 
     public void delete(E input) {
-        elements.remove(input);
         int key = input.hashCode();
         int hashValue = HashingFunctions.multiplyMatrix(hashMatrix, HashingFunctions.decimalToBinary(key)) % size;
         if (table[hashValue] != null) {
             if (table[hashValue].equals(input)) {
                 table[hashValue] = null;
                 System.out.println("Element removed");
-
+                itemCounter--;
             } else
                 System.out.println("Element not found");
-
         } else {
             System.out.println("Element not found");
         }
@@ -102,6 +123,7 @@ public class PerfectHashTableON2<E>{
                 System.out.println(table[i]);
             }
         }
+        System.out.println("Number Of Elements = "+itemCounter);
     }
 
     public String search(E input) {
@@ -119,23 +141,24 @@ public class PerfectHashTableON2<E>{
 
     public void batchInsertFromFile(String filePath) {
         int i = 0;
-        int oldSize = elements.size();
+        int oldSize = itemCounter;
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
-            while((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 i++;
             }
-            this.size =this.size+ (i * i);
+            this.size = this.size + (i * i);
             createPerfectHashTable();
         } catch (IOException e) {
-            
+
         }
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = br.readLine()) != null) {
                 insert((E) line);
             }
-            System.out.println("Number of duplicates: " + (-(elements.size()-oldSize - i)));
+            System.out.println("Number of duplicates: " + (-(itemCounter - oldSize - i)));
+            System.out.println("Inserted number of elements: " + (i+((itemCounter - oldSize - i))));
             System.out.println("Number of collisions: " + numberOfCollisions);
             this.numberOfCollisions = 0;
         } catch (IOException e) {
@@ -145,21 +168,22 @@ public class PerfectHashTableON2<E>{
 
     public void batchDeleteFromFile(String filePath) {
         int i = 0;
-        int oldSize = elements.size();
+        int oldSize = itemCounter;
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
-            while((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 i++;
             }
         } catch (IOException e) {
-            
+
         }
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = br.readLine()) != null) {
                 delete((E) line);
             }
-            System.out.println("Number of elements not found: " + (-(-elements.size()+oldSize - i)));
+            System.out.println("Number of elements not found: " + (-(-itemCounter + oldSize - i)));
+            System.out.println("Number of deleted items: "+(i-(-(-itemCounter + oldSize - i))));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -168,7 +192,14 @@ public class PerfectHashTableON2<E>{
     public int getCollisions() {
         return counter;
     }
-    public long getRebuildTime(){
+
+    public long getRebuildTime() {
         return time;
+    }
+    public int getSize(){
+        return size;
+    }
+    public int getNumberOfElements(){
+        return itemCounter;
     }
 }
